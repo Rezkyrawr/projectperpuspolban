ef3925dc06e9b4909534c953defb6058773de248
Nikosatria789 <satrianiko48@gmail.com>
2025-07-15 11:39:09 +0700

    Add files via upload

---
+++ app/Views/admin/layout/component/statistik.php
@@ -0,0 +1,66 @@
+ <div class="col-md-6 col-lg-3 mb-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-start">
+          <div class="avatar">
+            <img src="../assets/img/icons/unicons/chart-success.png" alt="Profit" class="rounded" />
+          </div>
+          <i class="bx bx-dots-vertical-rounded"></i>
+        </div>
+        <span class="fw-semibold d-block mb-1">Profit</span>
+        <h3 class="card-title mb-2">$12,628</h3>
+        <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +72.80%</small>
+      </div>
+    </div>
+  </div>
+
+  <!-- Sales -->
+  <div class="col-md-6 col-lg-3 mb-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-start">
+          <div class="avatar">
+            <img src="../assets/img/icons/unicons/wallet-info.png" alt="Sales" class="rounded" />
+          </div>
+          <i class="bx bx-dots-vertical-rounded"></i>
+        </div>
+        <span class="fw-semibold d-block mb-1">Sales</span>
+        <h3 class="card-title mb-2">$4,679</h3>
+        <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.42%</small>
+      </div>
+    </div>
+  </div>
+
+  <!-- Payments -->
+  <div class="col-md-6 col-lg-3 mb-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-start">
+          <div class="avatar">
+            <img src="../assets/img/icons/unicons/paypal.png" alt="Payments" class="rounded" />
+          </div>
+          <i class="bx bx-dots-vertical-rounded"></i>
+        </div>
+        <span class="fw-semibold d-block mb-1">Payments</span>
+        <h3 class="card-title mb-2">$2,456</h3>
+        <small class="text-danger fw-semibold"><i class="bx bx-down-arrow-alt"></i> -14.82%</small>
+      </div>
+    </div>
+  </div>
+
+  <!-- Transactions -->
+  <div class="col-md-6 col-lg-3 mb-4">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-start">
+          <div class="avatar">
+            <img src="../assets/img/icons/unicons/cc-primary.png" alt="Transactions" class="rounded" />
+          </div>
+          <i class="bx bx-dots-vertical-rounded"></i>
+        </div>
+        <span class="fw-semibold d-block mb-1">Transactions</span>
+        <h3 class="card-title mb-2">$14,857</h3>
+        <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
+      </div>
+    </div>
+  </div>
