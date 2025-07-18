023a352d11a6aefa04a74025f6c6e5152f712582
Nikosatria789 <satrianiko48@gmail.com>
2025-07-18 09:09:38 +0700

    Add files via upload

---
+++ app/Database/Migrations/2025-07-15-125008_CreateAdminTable.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateAdminTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_admin' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'unique'     => true,
+            ],
+            'password' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id_admin', true);
+        $this->forge->createTable('tb_admin'); // nama tabel `tb_admin`
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_admin');
+    }
+}
